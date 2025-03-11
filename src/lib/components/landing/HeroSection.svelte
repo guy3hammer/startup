1606b539f3f6825285b55a41ebbe205758d21a18
guy3hammer <guy3hammer@gmail.com>
2025-03-11 12:53:38 -0400

    Update HeroSection.svelte

--- src/lib/components/landing/HeroSection.svelte
+++ src/lib/components/landing/HeroSection.svelte
@@ -17,33 +17,22 @@
 </script>
 
 <section id="hero" class="relative mx-auto mt-32 max-w-7xl px-6 text-center md:px-8">
-	<div
-		class="backdrop-filter-[12px] group inline-flex h-7 -translate-y-4 animate-fade-in items-center justify-between gap-1 rounded-full border border-white/5 bg-white/10 px-3 text-xs text-white opacity-0 transition-all ease-in hover:cursor-pointer hover:bg-white/20 dark:text-black"
-	>
-		<AnimatedShinyText class="inline-flex items-center justify-center">
-			<span>✨ Schedule Free Consultation</span>
-			{' '}
-			<ArrowRightIcon
-				class="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5"
-			/>
-		</AnimatedShinyText>
-	</div>
+
 	<h1
 		class="-translate-y-4 animate-fade-in text-balance bg-gradient-to-br from-black from-30% to-black/40 bg-clip-text py-6 text-5xl font-medium leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] dark:from-white dark:to-white/40 sm:text-6xl md:text-7xl lg:text-8xl"
 	>
-		Torgen AI helps SMBs
+		Torgen helps SMBs
 		<br class="hidden md:block" />
 		{' '}
-		leverage open source AI
+		leverage Open Source AI
 	</h1>
 	<p
 		class="mb-12 -translate-y-4 animate-fade-in text-balance text-lg tracking-tight text-gray-400 opacity-0 [--animation-delay:400ms] md:text-xl"
 	>
-		Beautifully designed, animated components and templates built with
+		Take control of your data, escape lock-in, and empower your team
 		<br class="hidden md:block" />
 		{' '}
-		Tailwind CSS, <a href="https://svelte.dev" class="underline underline-offset-2">Svelte</a>, and
-		<a href="https://animation-svelte.vercel.app" class="underline underline-offset-2">Svelte Animations</a>.
+		with cutting-edge AI use cases for 27x lower cost.
 	</p>
 <AnimatedShinyText class="inline-flex items-center justify-center">
 			<span>✨ Schedule Free Consultation</span>
@@ -52,14 +41,7 @@
 				class="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5"
 			/>
 		</AnimatedShinyText>	
-<Button
-		class="-translate-y-4 animate-fade-in gap-1 rounded-lg text-white opacity-0 ease-in-out [--animation-delay:600ms] dark:text-black"
-	>
-		<span>Get Started for free </span>
-		<ArrowRightIcon
-			class="ml-1 size-4 transition-transform duration-300 ease-in-out group-hover:translate-x-1"
-		/>
-	</Button>
+
 	<div
 		use:inview={{
 			unobserveOnEnter: true,
