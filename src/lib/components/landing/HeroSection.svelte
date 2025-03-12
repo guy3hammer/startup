9ae3ae6d4c795de83a2defc9f91146f2cfc1f6be
guy3hammer <guy3hammer@gmail.com>
2025-03-12 12:05:21 -0400

    Update HeroSection.svelte

--- src/lib/components/landing/HeroSection.svelte
+++ src/lib/components/landing/HeroSection.svelte
@@ -29,7 +29,7 @@
 	<p
 		class="mb-12 -translate-y-4 animate-fade-in text-balance text-lg tracking-tight text-gray-400 opacity-0 [--animation-delay:400ms] md:text-xl"
 	>
-		Take control of your data, escape lock-in, and empower your team
+		Protect your data, escape lock-in, and empower your team
 		<br class="hidden md:block" />
 		{' '}
 		with cutting-edge AI use cases for 27x lower cost.
