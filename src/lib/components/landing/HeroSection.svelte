53b160d06b6dc67acd7af1c2c690b7121e80e214
guy3hammer <guy3hammer@gmail.com>
2025-03-13 16:27:12 -0400

    Update HeroSection.svelte

--- src/lib/components/landing/HeroSection.svelte
+++ src/lib/components/landing/HeroSection.svelte
@@ -14,6 +14,8 @@
 	import n8nSvg from '$lib/imgs/n8n-logo.svg';
 
 	// Images
+	import AppopsDarkImg from '$lib/imgs/appops-dark.png';
+	import AppopsLightImg from '$lib/imgs/appops-light.png';
 	import ITopsDarkImg from '$lib/imgs/itops-dark.png';
 	import ITopsLightImg from '$lib/imgs/itops-light.png';
 	import SecopsDarkImg from '$lib/imgs/secops-dark.png';
@@ -160,6 +162,42 @@
 
 
 <section id="hero" class="relative mx-auto mt-32 max-w-7xl px-6 text-center md:px-8">	
+
+<div
+		use:inview={{
+			unobserveOnEnter: true,
+			rootMargin: '-100px'
+		}}
+		on:inview_change={handleChange}
+		class="relative mt-32 animate-fade-up opacity-0 [--animation-delay:400ms] [perspective:2000px] after:absolute after:inset-0 after:z-50 after:[background:linear-gradient(to_top,hsl(var(--background))_30%,transparent)]"
+	>
+
+		<div
+			class="rounded-xl border border-white/10 bg-white bg-opacity-[0.01] before:absolute before:bottom-1/2 before:left-0 before:top-0 before:size-full before:opacity-0 before:[background-image:linear-gradient(to_bottom,var(--color-one),var(--color-one),transparent_40%)] before:[filter:blur(180px)] {inView
+				? 'before:animate-image-glow'
+				: ''}"
+		>
+			<BorderBeam
+				size={200}
+				duration={12}
+				delay={0}
+				colorFrom="var(--color-one)"
+				colorTo="var(--color-two)"
+			/>
+
+			<img
+				src={AppopsDarkImg}
+				alt="AppopsDarkImage"
+				class="relative hidden size-full rounded-[inherit] border object-contain dark:block"
+			/>
+			<img
+				src={AppopsLightImg}
+				alt="AppopsLightImage"
+				class="relative block size-full rounded-[inherit] border object-contain dark:hidden"
+			/>
+		</div>
+	</div>
+
 
 	<div
 		use:inview={{
