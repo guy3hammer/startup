210fafe6d101f84ca4a6a312a4394f7304234010
guy3hammer <guy3hammer@gmail.com>
2025-03-13 09:56:12 -0400

    Update HeroSection.svelte

--- src/lib/components/landing/HeroSection.svelte
+++ src/lib/components/landing/HeroSection.svelte
@@ -114,6 +114,11 @@
 
 <section id="hero" class="relative mx-auto mt-32 max-w-7xl px-6 text-center md:px-8">	
 
+	<div>
+		<h2 class="text-center text-sm font-semibold text-gray-600">
+				CREATE POWERFUL FLEXIBLE AUTOMATED WORKFLOWS IN SECONDS
+			</h2>
+	</div>
 	<div
 		use:inview={{
 			unobserveOnEnter: true,
@@ -122,9 +127,7 @@
 		on:inview_change={handleChange}
 		class="relative mt-32 animate-fade-up opacity-0 [--animation-delay:400ms] [perspective:2000px] after:absolute after:inset-0 after:z-50 after:[background:linear-gradient(to_top,hsl(var(--background))_30%,transparent)]"
 	>
-		<h2 class="text-center text-sm font-semibold text-gray-600">
-				CREATE POWERFUL FLEXIBLE AUTOMATED WORKFLOWS IN SECONDS
-			</h2>
+
 		<div
 			class="rounded-xl border border-white/10 bg-white bg-opacity-[0.01] before:absolute before:bottom-1/2 before:left-0 before:top-0 before:size-full before:opacity-0 before:[background-image:linear-gradient(to_bottom,var(--color-one),var(--color-one),transparent_40%)] before:[filter:blur(180px)] {inView
 				? 'before:animate-image-glow'
