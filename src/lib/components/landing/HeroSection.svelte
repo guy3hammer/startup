7c389247b0b4db929eb147e2de4c79d755631428
guy3hammer <guy3hammer@gmail.com>
2025-03-14 17:36:12 -0400

    Update HeroSection.svelte

--- src/lib/components/landing/HeroSection.svelte
+++ src/lib/components/landing/HeroSection.svelte
@@ -65,10 +65,10 @@
 	<div class="py-14">
 		<div class="mx-auto max-w-screen-xl px-4 md:px-8">
 			<h2 class="text-center text-sm font-semibold text-gray-600">
-				Tailor the <i>New Gen</i> OSAI models and agent-workflow
+				Tailor the <i>New Gen</i> OSAI models and automation
 			</h2>
 			<h2 class="text-center text-sm font-semibold text-gray-600">
-				automation tools to your specific needs, apps and data
+				tools to your specific needs, apps and data
 			</h2>
 		<br>
 		<br>
