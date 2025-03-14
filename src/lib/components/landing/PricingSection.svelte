cc32aca1b220350b3153298445a830cb8327ee54
guy3hammer <guy3hammer@gmail.com>
2025-03-14 17:12:32 -0400

    Update PricingSection.svelte

--- src/lib/components/landing/PricingSection.svelte
+++ src/lib/components/landing/PricingSection.svelte
@@ -67,8 +67,8 @@
 				<span><h2><strong>AI Innovation Lab</strong></h2></span>
 				</AnimatedShinyText>
 				</div><br>
-				<div><p><span><i>private managed AI innovation lab for</i></span></p>
-				<div><p><span><i>benchmarking testing and rapid prototyping</i></span></p>
+				<div><p><span><i>private managed AI innovation environment for model</i></span></p>
+				<div><p><span><i>benchmarking, use case testing and rapid prototyping</i></span></p>
 
 		</div> 	
 
